7181b4d31d5d3dd9e6796c05dc48bda1ce0ec6d5
Anshul Sharma <55247501+Anshul758@users.noreply.github.com>
2025-01-17 11:09:44 +0000

    string methods some more

--- Basics/strings.js
+++ Basics/strings.js
@@ -6,7 +6,7 @@ const repoCount = 50;
 //console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`); // more modern way it is called string interpolation
 
 // declaring string
-const gameName = new String('Anshul')
+const gameName = new String('Anshul-sh')
 
 // console.log(gameName[0]);
 // console.log(gameName.__proto__);
@@ -16,4 +16,8 @@ const gameName = new String('Anshul')
 console.log(gameName.charAt(2))
 console.log(gameName.indexOf('h'))
 
+const newString = gameName.substring(0,4);
+console.log(newString)
 
+const slicedString = gameName.slice(-6,-3);
+console.log(slicedString)
