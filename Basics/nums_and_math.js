054b1799a49294c5c87c2bffa1d78910ebe8cb31
Anshul Sharma <55247501+Anshul758@users.noreply.github.com>
2025-01-17 12:14:19 +0000

    numbers basic methods

---
+++ Basics/nums_and_math.js
@@ -0,0 +1,21 @@
+const balance = 400;
+// console.log(balance);
+
+const score = new Number(100); // this is how we can explicitly declare a type variable
+// console.log(score)
+
+// console.log(score.toString());
+// console.log(score.toFixed(1));
+
+const otherNumber = 123.889;
+
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// +++++ Maths +++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4)); // makes negative value into positive
+console.log(Math.round(4.4)); 
