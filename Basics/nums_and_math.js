2542fb6ab39761aa0b08d26eedf1f2a7ba88c7e7
Anshul Sharma <55247501+Anshul758@users.noreply.github.com>
2025-01-17 15:05:45 +0000

    math random method

--- Basics/nums_and_math.js
+++ Basics/nums_and_math.js
@@ -16,6 +16,21 @@ const hundreds = 1000000;
 
 // +++++ Maths +++++++++++++++
 
-console.log(Math);
-console.log(Math.abs(-4)); // makes negative value into positive
-console.log(Math.round(4.4)); 
+// console.log(Math);
+// console.log(Math.abs(-4)); // makes negative value into positive
+// console.log(Math.round(4.4)); 
+
+// random function
+
+console.log(Math.random());
+
+// math.random will give value between values 0 and 1 and doing multiplication with 10 so we can avoid 0 case
+console.log((Math.random()*10)+1);
+
+const min = 10;
+const max = 20;
+
+// math.random will give value between values 0 and 1 and doing multiplication with value so we can get result in range and added min
+// so atleast we get min value
+
+console.log(Math.floor(Math.random()*(max - min + 1)) + min);
