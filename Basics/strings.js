d7206c7cfc6e54b46cfd182ccf6abd1257a3f173
Anshul Sharma <55247501+Anshul758@users.noreply.github.com>
2025-01-17 07:29:15 +0000

    string inbuilt methods

---
+++ Basics/strings.js
@@ -0,0 +1,19 @@
+const name = "anshul";
+const repoCount = 50;
+
+//console.log(name + repoCount + " value"); not a good way to use this
+
+//console.log(`Hello my name is ${name} and my repoCount is ${repoCount}`); // more modern way it is called string interpolation
+
+// declaring string
+const gameName = new String('Anshul')
+
+// console.log(gameName[0]);
+// console.log(gameName.__proto__);
+
+// console.log(gameName.length)
+// console.log(gameName.toLocaleUpperCase())
+console.log(gameName.charAt(2))
+console.log(gameName.indexOf('h'))
+
+
